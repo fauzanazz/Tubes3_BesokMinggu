88a66744bf16c758ec48c99280f0df85fe180aec
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-05-09 22:19:54 +0700

    Init UI

---
+++ Tubes3_BesokMinggu/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tubes3_BesokMinggu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Tubes3_BesokMinggu/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Tubes3_BesokMinggu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg;*.bmp)|*.png;*.jpeg;*.jpg;*.bmp|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MyImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+        
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var text = System.IO.File.ReadAllText(openFileDialog.FileName);
+                ResultText.Text = text;
+            }
+        }
+        
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                System.IO.File.WriteAllText(saveFileDialog.FileName, ResultText.Text);
+            }
+        }
+        
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultText.ClearValue(System.Windows.Controls.TextBox.TextProperty);
+        }
+        
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
