397176dfaab6deffc862a603b57cd883578581e1
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-06-09 15:20:50 +0700

    Feat  : Threaeding

--- Tubes3_BesokMinggu/Solver.cs
+++ Tubes3_BesokMinggu/Solver.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
-using System.Windows.Shapes;
+using System.Threading.Tasks;
 using AForge.Imaging.Filters;
 using Rectangle = System.Drawing.Rectangle;
 
@@ -11,107 +10,63 @@ namespace Tubes3_BesokMinggu;
 
 public static class Solver
 {
-    private static int SIZE = 64;
+    private const int SIZE = 256;
     public static Database DB = new Database();
     
+    public static ResultData Solve(string path, Func<string, string, string, double> calculateSimilarity)
+    {
+        long start = DateTime.Now.Ticks;
+        Bitmap image = ProcessImage(path);
+        byte[] binary = ImageToByteArray(image);
+        byte[] croppedBinary = CropMiddleBinary(binary);
+        string ascii = BinaryToASCII(croppedBinary);
+        string fullBinary = BinaryToASCII(ImageToByteArray(image));
+
+        var result = new { nama = "", berkas_citra = "", Distance = 0.0 };
+        var highestsimsimage = "";
+        
+        var lockObject = new object();
+        bool stop = false;
+        
+        ParallelOptions parallelOptions = new ParallelOptions();
+        parallelOptions.MaxDegreeOfParallelism = Environment.ProcessorCount;
+
+        Parallel.ForEach(DB.sidik_jari, parallelOptions, (s, state) =>
+        {
+            if (stop) state.Stop();
+            double distance = calculateSimilarity(s.berkas_citra, ascii, fullBinary);
+            if (distance >= 100 || distance > result.Distance)
+            {
+                lock (lockObject)
+                {
+                    result = new { s.nama, s.berkas_citra, Distance = distance };
+                    highestsimsimage = s.berkas_citra;
+                }
+                if (distance == 100)
+                {
+                    stop = true;
+                    state.Stop();
+                }
+            }
+        });
+
+        var biodata = DB.ResultData
+            .AsEnumerable()
+            .FirstOrDefault(b => StringMatching.isMatch(result.nama, StringMatching.getBahasaAlayPattern(b.nama)));
+
+        long end = DateTime.Now.Ticks;
+        return new ResultData(biodata, new sidik_jari(){nama = result.nama, berkas_citra = result.berkas_citra}, (int)((end - start) / TimeSpan.TicksPerMillisecond), result.Distance, highestsimsimage);
+    }
+
     public static ResultData SolveBM(string path)
     {
-        long start = DateTime.Now.Ticks;
-        
-        // Load the image
-        Bitmap image = ProcessImage(path);
-        
-        // Convert the image to binary
-        byte[] binary = ImageToByteArray(image);
-        
-        // Crop the middle of the binary image
-        byte[] croppedBinary = CropMiddleBinary(binary);
-        
-        // Convert the binary to ASCII
-        string ascii = BinaryToASCII(croppedBinary);
-        
-        // Make the full binary string for levenshtein distance
-        string fullBinary = BinaryToASCII(ImageToByteArray(image));
-        
-        // Find the closest match in the database
-        var result = new { nama = "", berkas_citra = "", Distance = 0.0 };
-        var highestsimsimage = ""; // Save Output Image
-        foreach (var s in DB.sidik_jari)
-        {
-            var distance = CalculateBoyerMooreSimilarity(s.berkas_citra, ascii, fullBinary);
-            if (distance == 100)
-            {
-                result = new { s.nama, s.berkas_citra, Distance = distance };
-                highestsimsimage = s.berkas_citra;
-                break;
-            }
-            if (distance > result.Distance)
-            {
-                result = new { s.nama, s.berkas_citra, Distance = distance };
-                highestsimsimage = s.berkas_citra;
-            }
-        }
-        
-        // Find the biodata in the database based on the name
-        var biodata = DB.ResultData
-            .AsEnumerable()
-            .FirstOrDefault(b => StringMatching.isMatch(result.nama, StringMatching.getBahasaAlayPattern(b.nama)));
-        
-        long end = DateTime.Now.Ticks; // Calculate the time taken
-        
-        return new ResultData(biodata, new sidik_jari(){nama = result.nama, berkas_citra = result.berkas_citra}, (int)((end - start) / TimeSpan.TicksPerMillisecond), result.Distance, highestsimsimage);
+        return Solve(path, CalculateBoyerMooreSimilarity);
     }
-    
+
     public static ResultData SolveKMP(string path)
     {
-        long start = DateTime.Now.Ticks;
-        
-        // Load the image
-        Bitmap image = ProcessImage(path);
-        image.Save("p.png");
-        
-        // Convert the image to binary
-        byte[] binary = ImageToByteArray(image);
-        
-        // Crop the middle of the binary image
-        byte[] croppedBinary = CropMiddleBinary(binary);
-        
-        // Convert the binary to ASCII
-        string ascii = BinaryToASCII(croppedBinary);
-        
-        // Make the full binary string for levenshtein distance
-        string fullBinary = BinaryToASCII(ImageToByteArray(image));
-        
-        // Find the closest match in the database
-        var result = new { nama = "", berkas_citra = "", Distance = 0.0 };
-        var highestsimsimage = ""; // Save Output Image
-        foreach (var s in DB.sidik_jari)
-        {
-            double distance = CalculateKMPSimilarity(s.berkas_citra, ascii, fullBinary);
-            if (distance == 100)
-            {
-                result = new { s.nama, s.berkas_citra, Distance = distance };
-                highestsimsimage = s.berkas_citra;
-                break;
-            }
-            if (distance > result.Distance)
-            {
-                result = new { s.nama, s.berkas_citra, Distance = distance };
-                highestsimsimage = s.berkas_citra;
-            }
-        }
-        
-        // Find the biodata in the database based on the name
-        var biodata = DB.ResultData
-            .AsEnumerable()
-            .FirstOrDefault(b => StringMatching.isMatch(result.nama, StringMatching.getBahasaAlayPattern(b.nama)));
-        
-        long end = DateTime.Now.Ticks; // Calculate the time taken
-        
-        return new ResultData(biodata, new sidik_jari(){nama = result.nama, berkas_citra = result.berkas_citra}, (int)((end - start) / TimeSpan.TicksPerMillisecond), result.Distance, highestsimsimage);
-
+        return Solve(path, CalculateKMPSimilarity);
     }
-
     private static double CalculateKMPSimilarity(string text, string pattern, string fullPattern)
     {
         // Load txt file named text
@@ -120,8 +75,7 @@ public static class Solver
         
         if (number == -1) // Jika tidak ditemukan
         {
-            int levenshteinDistance = Algorithm.LevenshteinDistance(texts, fullPattern, SIZE);
-            return (SIZE - levenshteinDistance);
+            return CalculateLevenshteinDistance(texts, fullPattern);
         }
         
         return 100;
@@ -143,7 +97,7 @@ public static class Solver
     private static double CalculateLevenshteinDistance(string texts, string pattern)
     {
         int levenshteinDistance = Algorithm.LevenshteinDistance(texts, pattern, SIZE);
-        return (SIZE - levenshteinDistance);
+        return (double)(SIZE - levenshteinDistance) / SIZE;
     }
     
     public static Bitmap ProcessImage(String path)
